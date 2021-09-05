6b17e3a6984995c4e73836775b3a917f07c7213f
Fyodor Soikin <name.fa@gmail.com>
2021-09-05 17:30:40 -0400

    Reading history

--- src/Schedule/Weekdays.purs
+++ src/Schedule/Weekdays.purs
@@ -12,18 +12,20 @@ import Types (Kid(..), Subject(..))
 
 weekdays :: Rule
 weekdays date kid = append $ concat
-  [ for Matvey $ Maths `on` [Monday, Tuesday, Thursday, Friday]
-  , for Matvey $ Piano `on` [Tuesday, Wednesday, Friday]
+  [ for Matvey $ Piano `on` [Tuesday, Wednesday, Friday]
+  , for Matvey $ Maths `on` [Monday, Tuesday, Thursday, Friday]
   , for Matvey $ Chinese `on` [Thursday, Friday]
   , for Matvey $ VoiceTraining `on` [Tuesday, Thursday, Friday]
   , for Matvey $ Essay `on` [Monday, Tuesday, Wednesday]
   , for Matvey $ Spanish `on` [Tuesday, Friday]
 
-  , for Anya $ Maths `on` [Monday, Wednesday, Thursday, Friday]
   , for Anya $ Piano `on` [Monday, Tuesday, Wednesday, Friday]
+  , for Anya $ Maths `on` [Monday, Tuesday, Thursday, Friday]
   , for Anya $ Essay `on` [Monday, Wednesday]
   , for Anya $ Typing `on` [Tuesday, Thursday]
   , for Anya $ VoiceTraining `on` [Tuesday, Thursday, Friday]
+
+  , History `on` [Tuesday, Thursday]
   ]
   where
     on subj days
